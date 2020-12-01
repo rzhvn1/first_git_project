802ee974a8db55cc64860e2163aea1bb92f480d3
rzhvn1 <erzhanmuratov99@gmail.com>
2020-12-01 20:33:04 +0600

    HW_3

---
+++ ogogo_HW/HW_3/HW_3_1.py
@@ -0,0 +1,8 @@
+string = 'follow me please and so we can be friends'
+if 'f' in string:
+    print(string.index('f'))
+    print(string.rindex('f'))
+else:
+    print('this string doesnt have f')
+
+
