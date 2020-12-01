20560c7b1b1ed2226579a887b26fc711c145f508
rzhvn1 <erzhanmuratov99@gmail.com>
2020-12-01 20:41:09 +0600

    HW_3_2.py

--- ogogo_HW/HW_3/HW_3_1.py
+++ ogogo_HW/HW_3/HW_3_1.py
@@ -1,7 +1,6 @@
 string = 'follow me please and so we can be friends'
 if 'f' in string:
-    print(string.index('f'))
-    print(string.rindex('f'))
+    print(string.index('f'), string.rindex('f'))
 else:
     print('this string doesnt have f')
 
---
+++ ogogo_HW/HW_3_2/HW_3_2.py
@@ -0,0 +1,4 @@
+string = 'Ulan is the hottest student in AUCA and Akinai wants to reply to his message in Instagram'
+k = 0
+string = string.split()
+print(len(string))
